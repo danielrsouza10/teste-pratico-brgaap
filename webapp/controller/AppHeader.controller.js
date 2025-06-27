95b00346ac57cfc744bbc77b65613e186636997d
Daniel Souza <danielrsouza10@gmail.com>
2025-06-27 15:21:05 -0300

    fix: remocao de metodos sem uso

--- webapp/controller/AppHeader.controller.js
+++ webapp/controller/AppHeader.controller.js
@@ -1,21 +1,6 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], (Controller, MessageToast) => {
+], (Controller) => {
 	"use strict";
-
-	return Controller.extend("ui5.teste-pratico-brgaap.controller.AppHeader", {
-		async onOpenDialog() {
-			this.oDialog ??= await this.loadFragment({
-				name: "ui5.teste-pratico-brgaap.view.Dialog"
-			});
-
-			this.oDialog.open();
-		},
-
-		onCloseDialog() {
-			this.byId("Dialog").close();
-		}
-	});
-
+	return Controller.extend("ui5.teste-pratico-brgaap.controller.AppHeader", {});
 });
